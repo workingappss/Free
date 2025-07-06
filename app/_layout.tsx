573090c1d47674821a034ae052aa7d328af93053
workingappss <unicornlord123541@gmail.com>
2025-07-06 20:29:51 +0200

    Updated package.json

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,3 @@
-import { enableScreens } from 'react-native-screens';
-
-enableScreens();
-
 import { useEffect } from 'react';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
